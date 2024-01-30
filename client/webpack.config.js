bf87b8948767488be8d330782a54320077138e48
Wolffkran <liebherr2008@live.com>
2024-01-30 00:33:03 -0500

    Update paths in webpack.config.js

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -7,22 +7,22 @@ module.exports = () => {
   return {
     mode: 'production',
     entry: {
-      main: './src/js/index.js',
-      install: './src/js/install.js',
+      main: './client/src/js/index.js', // Update the path to your entry file
+      install: './client/src/js/install.js', // Update the path to your install file
     },
     output: {
       filename: '[name].bundle.js',
-      path: path.resolve(__dirname, 'dist'),
+      path: path.resolve(__dirname, 'client', 'dist'), // Update the output path
       publicPath: '/',
     },
     plugins: [
       new HtmlWebpackPlugin({
-        template: path.resolve(__dirname, 'src/index.html'), // Update the path to index.html
+        template: path.resolve(__dirname, 'client', 'src', 'index.html'), // Update the path to your index.html
         filename: 'index.html',
         chunks: ['main'],
       }),
       new HtmlWebpackPlugin({
-        template: path.resolve(__dirname, 'src/install.html'),
+        template: path.resolve(__dirname, 'client', 'src', 'install.html'), // Update the path to your install.html
         filename: 'install.html',
         chunks: ['install'],
       }),
@@ -36,7 +36,7 @@ module.exports = () => {
         display: 'standalone',
         icons: [
           {
-            src: path.resolve(__dirname, 'src/images/logo.png'),
+            src: path.resolve(__dirname, 'client', 'src', 'images', 'logo.png'), // Update the path to your icon
             sizes: [96, 128, 192, 256, 384, 512],
             destination: path.join('icons'),
           },
