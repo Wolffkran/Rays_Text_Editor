036de6e5db738af5013e23174b180b23596f2fcd
Wolffkran <liebherr2008@live.com>
2024-02-01 15:11:42 -0500

    change

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -3,58 +3,56 @@ const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
 
-module.exports = () => {
-  return {
-    mode: 'production',
-    entry: {
-      main: './src/js/index.js',
-      install: './src/js/install.js'
-    },
-    output: {
-      filename: 'bundle.js',
-      path: path.resolve(__dirname, 'dist'),
-    },
-    plugins: [
-      new HtmlWebpackPlugin({
-        template: './src/install.html',
-        filename: 'install.html',
-        chunks: ['install'],
-        inject: true,
-      }),
-      new HtmlWebpackPlugin({
-        template: './src/index.html',
-        filename: 'index.html',
-        chunks: ['main'],
-        inject: true,
-      }),
-      new WebpackPwaManifest({
-        name: 'Your PWA Name',
-        short_name: 'PWA',
-        description: 'Your PWA Description',
-        background_color: '#ffffff',
-        theme_color: '#000000',
-        crossorigin: 'use-credentials', // can be null, use-credentials, or anonymous
-        icons: [
-          {
-            src: path.resolve('src/images/logo.png'),
-            sizes: [96, 128, 192, 256, 384, 512],
-            destination: path.join('assets', 'icons'),
-          },
-        ],
-      }),
-      new InjectManifest({
-        swSrc: './client/src-sw.js', // Update the path if needed
-        swDest: 'service-worker.js',
-      }),
-    ],
-
-    module: {
-      rules: [
+module.exports = {
+  mode: 'production',
+  entry: {
+    main: './src/js/index.js',
+    install: './src/js/install.js'
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/install.html',
+      filename: 'install.html',
+      chunks: ['install'],
+      inject: true,
+    }),
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      filename: 'index.html',
+      chunks: ['main'],
+      inject: true,
+    }),
+    new WebpackPwaManifest({
+      name: 'Your PWA Name',
+      short_name: 'PWA',
+      description: 'Your PWA Description',
+      background_color: '#ffffff',
+      theme_color: '#000000',
+      crossorigin: 'use-credentials', 
+      icons: [
         {
-          test: /\.css$/,
-          use: ['style-loader', 'css-loader'],
+          src: path.resolve('src/images/logo.png'),
+          sizes: [96, 128, 192, 256, 384, 512],
+          destination: path.join('assets', 'icons'),
         },
       ],
-    },
-  };
+    }),
+    new InjectManifest({
+      swSrc: './client/src-sw.js', 
+      swDest: 'service-worker.js',
+    }),
+  ],
+
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
 };
