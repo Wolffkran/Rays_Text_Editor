eda1ed879e72df0a350026a0abd4b6882ff89a86
Wolffkran <liebherr2008@live.com>
2024-02-01 22:06:12 -0500

    change

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -3,56 +3,64 @@ const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
 
-module.exports = {
-  mode: 'production',
-  entry: {
-    main: path.resolve(__dirname, 'src/js/index.js'),
-    install: path.resolve(__dirname, 'src/js/install.js')
-  },
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, 'src/install.html'),
-      filename: 'install.html',
-      chunks: ['install'],
-      inject: true,
-    }),
-    new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, 'src/index.html'),
-      filename: 'index.html',
-      chunks: ['main'],
-      inject: true,
-    }),
-    new WebpackPwaManifest({
-      name: 'Your PWA Name',
-      short_name: 'PWA',
-      description: 'Your PWA Description',
-      background_color: '#ffffff',
-      theme_color: '#000000',
-      crossorigin: 'use-credentials',
-      icons: [
+module.exports = () => {
+  return {
+    mode: 'development',
+    entry: {
+      main: './src/js/index.js',
+      install: './src/js/install.js'
+    },
+    output: {
+      filename: '[name].bundle.js',
+      path: path.resolve(__dirname, 'dist'),
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: './index.html',
+        title: 'J.A.T.E'
+      }),
+      new InjectManifest({
+        swSrc: './src-sw.js',
+        swDest: 'src-sw.js',
+      }),
+      new WebpackPwaManifest({
+        fingerprints: false,
+        inject: true,
+        name: 'Just Another Text Editor',
+        short_name: 'J.A.T.E',
+        description: 'Takes notes with JavaScript syntax highlighting!',
+        background_color: '#225ca3',
+        theme_color: '#225ca3',
+        start_url: '/',
+        publicPath: '/',
+        icons: [
+          {
+            src: path.resolve('src/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join('assets', 'icons'),
+          },
+        ],
+      }),
+    ],
+
+    module: {
+      rules: [
         {
-          src: path.resolve(__dirname, 'src/images/logo.png'),
-          sizes: [96, 128, 192, 256, 384, 512],
-          destination: path.join('assets', 'icons'),
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+              plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
+            },
+          },
         },
       ],
-    }),
-    new InjectManifest({
-      swSrc: path.resolve(__dirname, 'src-sw.js'),
-      swDest: 'service-worker.js',
-    }),    
-  ],
-
-  module: {
-    rules: [
-      {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader'],
-      },
-    ],
-  },
+    },
+  };
 };
